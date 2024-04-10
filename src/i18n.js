bd7898ac1e3e7e99b8e1ae50c1e56f4c1bbb4eeb
Devloner <tminecraftben@gmail.com>
2024-04-10 22:28:32 +0300

    add area-depend localization

--- src/i18n.js
+++ src/i18n.js
@@ -4,6 +4,7 @@ import enTranslation from './localization/en.json';
 import ukTranslation from './localization/uk.json';
 import deTranslation from './localization/de.json';
 import esTranslation from './localization/es.json';
+import axios from 'axios';
 
 import 'dayjs/locale/de';
 import 'dayjs/locale/en';
@@ -18,25 +19,41 @@ const resources = {
   es: { translation: esTranslation },
 };
 
-const defLang = ()=>{
-if(window.localStorage.defaultLanguage== undefined)
-return 'en';
-else {
-  dayjs.locale(window.localStorage.defaultLanguage);
-  return window.localStorage.defaultLanguage;
-}
-}
+const defLang = () => {
+  if (window.localStorage.defaultLanguage == undefined) return 'en';
+  else {
+    dayjs.locale(window.localStorage.defaultLanguage);
+    return window.localStorage.defaultLanguage;
+  }
+};
 
+const getLanguageByDefault = async () => {
+  console.log('test');
+  const country = (
+    await axios.get('https://ipinfo.io/')
+  ).data.country.toLowerCase();
+  const dict = new Map();
+  dict.set('ua', 'uk');
+  dict.set('de', 'de');
+  dict.set('es', 'es');
+  console.log('rece', country, dict.get(country));
+  return dict.get(country);
+};
 
-i18n
-  .use(initReactI18next)
-  .init({
-    lng: defLang(),
-    fallbackLng: "en",
-    resources,
-    interpolation: {
-      escapeValue: false // не потрібно екранувати значення
-    }
+if (window.localStorage.defaultLanguage == undefined)
+  getLanguageByDefault().then((lang) => {
+    console.log('change to lang', lang);
+    i18n.changeLanguage(lang);
+    window.localStorage.defaultLanguage = lang;
   });
+
+i18n.use(initReactI18next).init({
+  lng: defLang(),
+  fallbackLng: 'en',
+  resources,
+  interpolation: {
+    escapeValue: false, // не потрібно екранувати значення
+  },
+});
 
 export default i18n;
